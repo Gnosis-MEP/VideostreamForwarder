12c32bb2ca2998b9bf90cb14a6568c3c835c2fe3
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2023-02-17 14:27:49 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='videostreamforwarder',
+    version='0.1',
+    description='Forwards to the subscriber query a video stream (HLS) based on the query id.',
+    author='Felipe Arruda Pontes',
+    author_email='felipe.arruda.pontes@insight-centre.org',
+    packages=['videostreamforwarder'],
+    zip_safe=False
+)
